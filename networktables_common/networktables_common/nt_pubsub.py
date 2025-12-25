c7f5ec4ef9f90092c8481c8971c5a78c7cc384db
WinstonHChou <winston.h.chou@gmail.com>
2025-12-24 19:03:38 -0800

    Add Python node and installation for TestNTCommonNode with NetworkTables integration

--- networktables_common/networktables_common/nt_pubsub.py
+++ networktables_common/networktables_common/nt_pubsub.py
@@ -1,53 +1,142 @@
 # Copyright (c) 2025 C.K. Robotics
 
-import os
-from ament_index_python import get_package_share_directory
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PythonExpression
+import ntcore as nt
+from typing import Any
 
 
-def find_exe_preferring_local(local_exe, installed_exe):
-    return local_exe if os.path.isfile(local_exe) else installed_exe
+kDefaultPubSubOptions = nt.PubSubOptions()
+kSensorPubSubOptions = nt.PubSubOptions(periodic=0.02)  # 50 Hz
 
+TopicValueType: dict[type, Any] = {
+  nt.BooleanArrayTopic: list[bool],
+  nt.BooleanTopic: bool,
+  nt.DoubleArrayTopic: list[float],
+  nt.DoubleTopic: float,
+  nt.FloatArrayTopic: list[float],
+  nt.FloatTopic: float,
+  nt.IntegerArrayTopic: list[int],
+  nt.IntegerTopic: int,
+  nt.RawTopic: bytearray,
+  nt.StringArrayTopic: list[str],
+  nt.StringTopic: str,
+  # no protobuf topic
+  nt.StructArrayTopic: list[bytearray],
+  nt.StructTopic: bytearray,
+}
 
-def get_share_file(package_name, *args):
-    return os.path.join(get_package_share_directory(package_name), *args)
+# TODO(Winston): consider restricting TopicT and msg types
+class TopicPublisher:
+  def __init__(self, TopicT,
+               inst: nt.NetworkTableInstance,
+               topic_name: str,
+               options: nt.PubSubOptions = kDefaultPubSubOptions):
+    self.__inst = inst
+    self.__latest_msg_time = nt._now()
 
+    name = topic_name[(topic_name.rfind("/")) + 1:]
+    table_name = topic_name[:(topic_name.rfind("/"))]
 
-def get_sim_time_launch_arg():
-    use_sim_time = LaunchConfiguration("use_sim_time")
+    self.__publisher = TopicT(self.__inst.getTable(table_name).getTopic(name)).publish(options)
+    self.__activated = False
 
-    declare_use_sim_time_cmd = DeclareLaunchArgument(
-        "use_sim_time", default_value="False", description="Use simulation clock if True"
+  def publish(self, msg):
+    self.__publisher.set(msg)
+    self.__latest_msg_time = nt._now()
+
+  def latest_msg_time(self) -> int:
+    return self.__latest_msg_time
+
+  def on_activate(self) -> bool:
+    self.__activated = True
+    return True
+
+  def on_deactivate(self) -> bool:
+    self.__activated = False
+    return True
+
+  def is_activated(self) -> bool:
+    return self.__activated
+
+class TopicSubscriber:
+  def __init__(self, TopicT,
+               inst: nt.NetworkTableInstance,
+               topic_name: str,
+               callback = None,
+               options: nt.PubSubOptions = kDefaultPubSubOptions,
+               default_msg = None):
+    self.__inst = inst
+    self.__latest_msg_time = nt._now()
+    self.__callback = callback
+    self.__last_received_msg = None
+    self.__has_seen_msg = False
+
+    name = topic_name[(topic_name.rfind("/")) + 1:]
+    table_name = topic_name[:(topic_name.rfind("/"))]
+
+    if default_msg is None:
+      default_msg = TopicValueType[TopicT]()
+    elif type(default_msg) == TopicValueType[TopicT]:
+      pass
+    else:
+      raise TypeError("default_msg type does not match TopicT value type"
+                      "while creating TopicSubscriber")
+
+    self.__subscription = TopicT(self.__inst.getTable(table_name).getTopic(name)).subscribe(default_msg, options)
+
+    self.__value_listener_handle = self.__inst.addListener(
+      self.__subscription, nt.EventFlags.kValueAll, self.__event_callback
     )
 
-    return declare_use_sim_time_cmd, {"use_sim_time": use_sim_time}
+  def __event_callback(self, event: nt.Event):
+    msg = self.__subscription.get()
+    self.__on_msg_received(msg)
+    if self.__callback is not None:
+      self.__callback(msg)
 
+  def __on_msg_received(self, msg):
+    self.__has_seen_msg = True
+    self.__last_received_msg = msg
+    self.__latest_msg_time = nt._now()
 
-def to_lower(launch_configuration):
-    return PythonExpression(["'", launch_configuration, "'", ".lower()"])
+  def __del__(self):
+    self.__inst.removeListener(self.__value_listener_handle)
 
+  def take(self):
+    msg = self.__last_received_msg.copy()
+    self.__last_received_msg = None
+    return msg
 
-def check_val_in_list(val, list_to_check):
-    return PythonExpression(["'", LaunchConfiguration(val), "' in ", str(list_to_check)])
+  def has_seen_msg(self) -> bool:
+    return self.__has_seen_msg
 
+  def has_msg(self) -> bool:
+    return self.__last_received_msg is not None
 
-def get_param_file(package_name, param_base=None, launch_config_var="race_type", prefix=False):
-    if param_base is None:
-        param_base = package_name
-    if prefix:
-        config = (
-            get_share_file(package_name),
-            "/param/",
-            to_lower(LaunchConfiguration(launch_config_var)),
-            "/",
-            f"{param_base}.param.yaml",
-        )
-    else:
-        config = (
-            get_share_file(package_name),
-            f"/param/{param_base}_",
-            to_lower(LaunchConfiguration(launch_config_var)),
-            ".param.yaml",
-        )
-    return config
+  def latest_msg_time(self) -> int:
+    return self.__latest_msg_time
+
+  def latest_received_msg(self):
+    return self.__last_received_msg
+
+def publish_to(TopicT, inst: nt.NetworkTableInstance,
+               publisher: TopicPublisher,
+               topic_name: str,
+               activate: bool = False,
+               options: nt.PubSubOptions = kDefaultPubSubOptions):
+  publisher = TopicPublisher(TopicT, inst, topic_name, options)
+  if activate:
+    publisher.on_activate()
+
+def activate_publisher(publisher: TopicPublisher):
+  publisher.on_activate()
+
+def deactivate_publisher(publisher: TopicPublisher):
+  publisher.on_deactivate()
+
+def subscribe_from(TopicT, inst: nt.NetworkTableInstance,
+                   subscriber: TopicSubscriber,
+                   topic_name: str,
+                   callback = None,
+                   options: nt.PubSubOptions = kDefaultPubSubOptions,
+                   default_msg = None):
+  subscriber = TopicSubscriber(TopicT, inst, topic_name, callback, options, default_msg)
